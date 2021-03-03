43def3862a5023b8a16039bec242b93e0ae35f4f
Justin Bush <revblaze@users.noreply.github.com>
2021-03-03 10:02:45 -0500

    Create Cocoa project

---
+++ Cocoa/SimpleSidebar/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  SimpleSidebar
+//
+//  Created by Justin Bush on 2021-03-03.
+//
+
+import Cocoa
+
+@main
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ Cocoa/SimpleSidebar/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  SimpleSidebar
+//
+//  Created by Justin Bush on 2021-03-03.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
