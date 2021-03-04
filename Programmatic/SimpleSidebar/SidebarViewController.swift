0de6266c893c497a1981f236f331390671b951e2
Justin Bush <revblaze@users.noreply.github.com>
2021-03-04 10:14:06 -0500

    Implement enum-based Sidebar

--- Programmatic/SimpleSidebar/SidebarViewController.swift
+++ Programmatic/SimpleSidebar/SidebarViewController.swift
@@ -12,7 +12,8 @@ class SidebarViewController: NSViewController {
     @IBOutlet var sidebar: NSOutlineView!
     
     // Dummy data used for row titles
-    let items = ["Welcome", "Home", "Websites", "Domains", "Templates"]
+    let items = ["Discover", "Arcade", "Create", "Work", "Play", "Updates", "Develop", "Categories", "Updates"]
+    let icons = ["Rocket", "Rocket", "Rocket", "Rocket", "Rocket", "Rocket", "Rocket", "Rocket", "Rocket"]
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -21,18 +22,57 @@ class SidebarViewController: NSViewController {
 //        NotificationCenter.default.addObserver(self, selector: #selector(windowLostFocus), name: NSApplication.willResignActiveNotification, object: nil)
 //        NotificationCenter.default.addObserver(self, selector: #selector(windowGainedFocus), name: NSApplication.willBecomeActiveNotification, object: nil)
     }
+}
+
+enum SidebarMenu: Int, CaseIterable {
+    case discover
+    case arcade
+    case create
+    case work
+    case play
+    case develop
+    case categories
+    case updates
     
+    var title: String {
+        switch self {
+        case .discover: return "Discover"
+        case .arcade:  return "Arcade"
+        case .create: return "Create"
+        case .work: return "Work"
+        case .play: return "Play"
+        case .develop: return "Develop"
+        case .categories: return "Categories"
+        case .updates: return "Updates"
+        }
+    }
+    
+    var icon: NSImage {
+        switch self {
+        case .discover: return NSImage(named: "icon-rocket")!
+        case .arcade:  return NSImage(named: "icon-rocket")!
+        case .create: return NSImage(named: "icon-rocket")!
+        case .work: return NSImage(named: "icon-rocket")!
+        case .play: return NSImage(named: "icon-rocket")!
+        case .develop: return NSImage(named: "icon-rocket")!
+        case .categories: return NSImage(named: "icon-rocket")!
+        case .updates: return NSImage(named: "icon-rocket")!
+        }
+    }
 }
 
 extension SidebarViewController: NSOutlineViewDataSource {
     // Number of items in the sidebar
     func outlineView(_ outlineView: NSOutlineView, numberOfChildrenOfItem item: Any?) -> Int {
-        return items.count
+        //return items.count
+        return SidebarMenu.allCases.count
     }
     
     // Items to be added to sidebar
     func outlineView(_ outlineView: NSOutlineView, child index: Int, ofItem item: Any?) -> Any {
-        return items[index]
+        //return items[index]
+        return SidebarMenu(rawValue: index) as Any
+        
     }
     
     // Whether rows are expandable by an arrow
@@ -98,20 +138,22 @@ extension SidebarViewController: NSOutlineViewDataSource {
 }
 
 extension SidebarViewController: NSOutlineViewDelegate {
-  
-  func outlineView(_ outlineView: NSOutlineView, viewFor tableColumn: NSTableColumn?, item: Any) -> NSView? {
-    var view: NSTableCellView?
     
-    if let title = item as? String {
-      view = outlineView.makeView(withIdentifier: NSUserInterfaceItemIdentifier(rawValue: "ItemCell"), owner: self) as? NSTableCellView
-      if let textField = view?.textField {
-        textField.stringValue = title
-        textField.sizeToFit()
-      }
+    func outlineView(_ outlineView: NSOutlineView, viewFor tableColumn: NSTableColumn?, item: Any) -> NSView? {
+        var view: NSTableCellView?
+        
+        if let menuItem = item as? SidebarMenu {
+            view = outlineView.makeView(withIdentifier: NSUserInterfaceItemIdentifier(rawValue: "ItemCell"), owner: self) as? NSTableCellView
+            if let textField = view?.textField {
+                textField.stringValue = menuItem.title
+            }
+            if let imageView = view?.imageView {
+                imageView.image = menuItem.icon
+            }
+        }
+        
+        return view
     }
     
-    return view
-  }
-  
 }
 
